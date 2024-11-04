c3db502f2f07a1bd3a819ab3ec8890703b8cd5df
Nikolaj Gordienko <nicolas.gordienko@gmail.com>
2024-11-04 20:41:21 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    logging.info("Hello World")
+
+if __name__ == "__main__":
+    main()
